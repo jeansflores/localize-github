c432dc1a94c448225dcffb1a1bc6409b7a3002ba
Jean <jeansfloresof@gmail.com>
2020-03-11 00:15:23 -0300

    Adiciona estilos globais da aplicação

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,24 +2,31 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    outline: 0;
     box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
   }
 
-  html, body, #root {
-    min-height: 100%;
+  html, body {
+    height: 100vh;
+    overflow: auto;
   }
 
   body {
     -webkit-font-smoothing: antialiased !important;
+    background-color: #24292e;
+  }
+
+  #root {
+    display: flex;
+    justify-content: center;
   }
 
   body, input, button {
     color: #222;
-    font-size: 14px;
     font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
   }
 
   button {
---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export const colors = {
+  black: '#000000',
+  white: '#ffffff',
+}
+
+export const theme = Object.freeze({
+  colors: {
+    danger: '#dc3545',
+    dangerLighter: '#f8d7da',
+    dark: '#343a40',
+    darkLighter: '#d6d8d9',
+    info: '#17a2b8',
+    infoLighter: '#d1ecf1',
+    light: '#f8f9fa',
+    lighter: '#fefefe',
+    primary: '#007bff',
+    primaryLighter: '#cce5ff',
+    secondary: '#6c757d',
+    secondaryLighter: '#e2e3e5',
+    success: '#28a745',
+    successLighter: '#d4edda',
+    warning: '#ffc107',
+    warningLighter: '#fff3cd',
+  },
+})
